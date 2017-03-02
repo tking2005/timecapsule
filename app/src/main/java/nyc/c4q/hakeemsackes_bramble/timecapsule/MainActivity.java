f6fd029d378fb4c0daae720c3e95e082f9661e5c
catcattac <catwong.nyc@gmail.com>
2017-03-02 17:11:33 -0500

    edit sign in/sign up views

--- app/src/main/java/nyc/c4q/hakeemsackes_bramble/timecapsule/MainActivity.java
+++ app/src/main/java/nyc/c4q/hakeemsackes_bramble/timecapsule/MainActivity.java
@@ -2,12 +2,19 @@ package nyc.c4q.hakeemsackes_bramble.timecapsule;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
